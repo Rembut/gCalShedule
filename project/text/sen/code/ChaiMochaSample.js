20af4a9a29f9e914278e47767b86c055d07f297c
SenDen <senden1998@gmail.com>
2019-05-16 20:54:01 +0300

    Text added
    Text was added;
    Some changes in titlepage.

---
+++ project/text/sen/code/ChaiMochaSample.js
@@ -0,0 +1,10 @@
+const mocha = require('mocha');
+const chai = require('chai');
+
+const expect = chai.expect;
+
+describe('Reverse String Test', () => {
+    it('Checks if the strings is reversed', () => {
+        // Code 
+    });
+});
